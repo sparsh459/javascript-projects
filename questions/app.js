035afd5a0c9c8277cd0398f87fca47621319fdee
Sparsh <sparsh0987654321@gmail.com>
2021-11-28 17:19:24 +0530

    ADDED FAQ part

---
+++ questions/app.js
@@ -0,0 +1,34 @@
+//using selectors inside the element
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+  // console.log(btn);
+
+  btn.addEventListener("click", function () {
+    // console.log(question);
+
+    // this does the trick of close others when one is opened
+    // iterating the list of all questions and checks that if not the question whose button clicked, then close the article 
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+
+    question.classList.toggle("show-text");
+  });
+});
+
+
+// other way if doing the same thing written above
+// traversing the dom
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//     const question = e.currentTarget.parentElement.parentElement;
+
+//     question.classList.toggle("show-text");
+//   });
+// });
